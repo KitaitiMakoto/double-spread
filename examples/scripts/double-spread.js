f2778ad753ce82c72a34f78b1762c2194df6c038
KITAITI Makoto <KitaitiMakoto@gmail.com>
2011-09-12 22:19:13 +0900

    Add lib file to example directory

--- examples/scripts/double-spread.js
+++ examples/scripts/double-spread.js
@@ -1,1 +1,59 @@
-lib/double-spread.js
+DoubleSpread = {
+    Controller: {
+        pages: 2,
+        views: [],
+        back: function(number) {
+            if (number === undefined)
+                number = this.pages;
+            for (var i in this.views) {
+                var view = this.views[i];
+                // To do: try and catch exception thrown by model
+                view.model = view.model.previous(number);
+	    }
+            this.refresh();
+        },
+        forward: function(number) {
+            if (number === undefined)
+                number = this.pages;
+            for (var i in this.views) {
+                var view = this.views[i];
+                // To do: try and catch exception thrown by model
+                view.model = view.model.next(number);
+	    }
+            this.refresh();
+        },
+        refresh: function() {
+            for (var i in this.views) {
+                this.views[i].render();
+	    }
+	},
+    },
+
+    Model: function(id) {
+        // Dummy implementation
+        this.id = id;
+        this.content = "<h1>Page " + this.id + "</h1>";
+    },
+
+    View: function($node) {
+        this.$node = $node;
+        this.model = null;
+    }
+};
+DoubleSpread.Model.prototype.previous = function(number) {
+    // Dummey implementation
+    // Should throw exception when no page
+    if (number === undefined)
+        number = 1;
+    return new DoubleSpread.Model(this.id - number);
+}
+DoubleSpread.Model.prototype.next = function(number) {
+    // Dummey implementation
+    // Should throw exception when no page
+    if (number === undefined)
+        number = 1;
+    return new DoubleSpread.Model(this.id + number);
+}
+DoubleSpread.View.prototype.render = function() {
+    this.$node.html(this.model.content);
+};
